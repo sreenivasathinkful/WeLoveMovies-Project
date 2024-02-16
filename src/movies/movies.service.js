8cdc097da66d5d956e30c6eae3bb2a1c073f7c1b
Sreenivasathinkful <146571389+sreenivasathinkful@users.noreply.github.com>
2024-02-16 14:01:20 -0800

    Update movies.service.js

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,5 +1,12 @@
 const db = require("../db/connection");
 
+/**
+ * Lists movies based on whether they are currently showing.
+ * Gets movies and related theaters data.
+ * Gets movies review data.
+ * Joins movies, reviews, and critics tables.
+ * Gets a single movie by id.
+ */
 async function list(is_showing) {
   return db("movies")
     .select("movies.*")
@@ -17,25 +24,35 @@ async function list(is_showing) {
     });
 }
 
+/**
+ * Joins movies, movies_theaters, and theaters tables to get movies and related theaters data.
+ * Selects all columns from the joined tables.
+ */
 async function getMoviesTheaters() {
   return db("movies_theaters as mt")
     .join("movies as m", "mt.movie_id", "m.movie_id")
     .join("theaters as t", "mt.theater_id", "t.theater_id")
-    .select("*")
+    .select("*");
 }
 
+/**
+ * Joins movies, reviews and critics tables to get movies reviews data.
+ * Selects all columns from the joined tables.
+ */
 async function getMoviesReviews() {
   return db("reviews as r")
     .join("movies as m", "r.movie_id", "m.movie_id")
     .join("critics as c", "c.critic_id", "r.critic_id")
-    .select("*")
+    .select("*");
 }
 
+/**
+ * Gets a single movie by id.
+ * @param {number} movie_id - The id of the movie to get.
+ * @returns {Object} The movie data.
+ */
 async function read(movie_id) {
-    return db("movies")
-      .select("*")
-      .where({movie_id})
-      .first()
+  return db("movies").select("*").where({ movie_id }).first();
 }
 
 module.exports = {
