d7e5e9178219747d6d6b88b09a7a1f3b7fbbdc20
Sreenivasathinkful <146571389+sreenivasathinkful@users.noreply.github.com>
2024-02-16 14:02:03 -0800

    Update theaters.controller.js

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,6 +1,10 @@
 const service = require("./theaters.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
+/**
+ * Lists all theaters.
+ * @returns {Promise} A promise that resolves with an array of theaters.
+ */
 async function list(request, response) {
   const list = await service.list();
   response.status(200).json({ data: list });
